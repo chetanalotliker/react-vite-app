b6c4a679f162e9b0ac199b010fe46a73a787868b
chetana.lotliker <chetana.lotliker2@gmail.com>
2025-04-03 23:29:56 +0530

    Create React Vite project, create component, pass props

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import SimpleTitle from './SimpleTitle'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <SimpleTitle name="mockname"></SimpleTitle>
+        <SimpleTitle name="sample name"></SimpleTitle>
+        <SimpleTitle name="dummy user"></SimpleTitle>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/SimpleTitle.jsx
@@ -0,0 +1,5 @@
+function SimpleTitle(props){
+return <h2>Welcome to the react-vite-app project {props.name}</h2>
+}
+
+export default SimpleTitle; 
